c84449d773c2fc308987466c119a038ec6537903
LamNguyen <9.4ngoclam@gmail.com>
2021-08-25 15:07:51 +0700

    fix some code

--- src/google/google.controller.ts
+++ src/google/google.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Param } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { GoogleService } from './google.service';
 
 @ApiTags('Google')
@@ -8,13 +7,12 @@ import { GoogleService } from './google.service';
 export class GoogleController {
   constructor(private readonly googleService: GoogleService) {}
 
-  @Get()
-  @UseGuards(AuthGuard('google'))
-  async googleAuth(@Req() req) {}
-
-  @Get('redirect')
-  @UseGuards(AuthGuard('google'))
-  googleAuthRedirect(@Req() req) {
-    return this.googleService.googleLogin(req);
+  @Get(':tokenId')
+  @ApiResponse({
+    status: 200,
+    description: 'login with google by sending tokenId',
+  })
+  checkTokenId(@Param('tokenId') tokenId: string) {
+    this.googleService.googleLogin(tokenId);
   }
 }
