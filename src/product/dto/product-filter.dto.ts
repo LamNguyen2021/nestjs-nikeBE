d4c43646c8dc13071afd6f59e40fa1c3c13a4179
LamNguyen <9.4ngoclam@gmail.com>
2021-08-26 17:00:02 +0700

    change method and decorator at findWithFilter function, product.service.ts

--- src/product/dto/product-filter.dto.ts
+++ src/product/dto/product-filter.dto.ts
@@ -1,15 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class ProductFilterDto {
-  @ApiProperty({ type: String })
+  @ApiProperty({ type: String, required: false })
   name: string;
 
-  @ApiProperty({ type: [String] })
+  @ApiProperty({ type: [String], required: false })
   genderId: string[];
 
-  @ApiProperty({ type: [String] })
+  @ApiProperty({ type: [String], required: false })
   colorId: string[];
 
-  @ApiProperty({ type: [String] })
+  @ApiProperty({ type: [String], required: false })
   sizeId: string[];
 }
