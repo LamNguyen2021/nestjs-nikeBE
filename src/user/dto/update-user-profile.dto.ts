9e3ddbe61e5bdab4a587c9dc43fefb98656a88a2
LamNguyen <9.4ngoclam@gmail.com>
2021-08-27 20:56:15 +0700

    create-update user profile, update password

---
+++ src/user/dto/update-user-profile.dto.ts
@@ -0,0 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsInt } from 'class-validator';
+import { IsNotBlank } from 'src/custom-validator/is-not-blank.validator';
+
+export class UpdateUserProfileDto {
+  @IsNotBlank('username', { message: 'username can not empty' })
+  @ApiProperty({ type: String })
+  username: string;
+
+  @IsEmail()
+  @ApiProperty({ type: String })
+  email: string;
+
+  @IsNotBlank('name', { message: 'name can not empty' })
+  @ApiProperty({ type: String })
+  name: string;
+
+  @IsInt()
+  @ApiProperty({ type: Number })
+  yearOfBirth: number;
+
+  @ApiProperty({ type: String })
+  address: string;
+}
